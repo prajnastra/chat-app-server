840a6e24303f5571042ef8697837bf749393cf92
prajnastra <coder.abhijit90@gmail.com>
2023-02-01 16:57:42 +0530

    add: prettierrc & combine: socket & gun

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-const port = process.env.PORT || 8000;
-
-console.log(`Socket server running at: ${port}`);
-
-const io = require("socket.io")(port, {
+const app = require('express')()
+const Gun = require('gun')
+const http = require('http').Server(app)
+const io = require('socket.io')(http, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
-});
+})
 
-io.on("connection", (socket) => {
-  const id = socket.handshake.query.id;
-  socket.join(id);
+io.on('connection', (socket) => {
+  const id = socket.handshake.query.id
+  socket.join(id)
 
-  socket.on("send-message", ({ recipients, text }) => {
+  socket.on('send-message', ({ recipients, text }) => {
     recipients.forEach((recipient) => {
-      const newRecipients = recipients.filter((r) => r !== recipient);
-      newRecipients.push(id);
-      socket.broadcast.to(recipient).emit("receive-message", {
+      const newRecipients = recipients.filter((r) => r !== recipient)
+      newRecipients.push(id)
+      socket.broadcast.to(recipient).emit('receive-message', {
         recipients: newRecipients,
         sender: id,
         text,
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})
+
+
+app.use(Gun.serve)
+
+const port = process.env.PORT || 8000
+
+const server = http.listen(port, function () {
+  console.log(`Socket listening on ${port}`)
+  console.log(`Gun listening on ${port}`)
+})
+
+Gun({ web: server })
